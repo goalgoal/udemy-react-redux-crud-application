d478985a4ddfaeab28bce1c376a2cd9a79bb5277
goalgoal <m.inaba0625@gmail.com>
2020-10-20 21:32:23 +0900

    laern jsx sintax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello,world</div>;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked!")}}></input>
+    </React.Fragment>
+  )
 }
 
 export default App;
