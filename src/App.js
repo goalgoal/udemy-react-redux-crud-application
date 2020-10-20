2e3ac3e7045f3621f915c1983cb866d44a1dbcee
goalgoal <m.inaba0625@gmail.com>
2020-10-20 22:13:18 +0900

    create functional compornent

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked!")}}></input>
-    </React.Fragment>
+    <div>Hi!</div>
   )
 }
 
